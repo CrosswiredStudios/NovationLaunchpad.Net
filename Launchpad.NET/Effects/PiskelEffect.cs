f36d9d252fbb65112fb0808ad9661fb2fd457f2c
Matthew Wood <mattwood2855@gmail.com>
2017-10-14 12:13:06 -0700

    piskel effect

--- Launchpad.NET/Effects/PiskelEffect.cs
+++ Launchpad.NET/Effects/PiskelEffect.cs
@@ -1,15 +1,23 @@
 ﻿using Newtonsoft.Json.Linq;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reactive;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 namespace Launchpad.NET.Effects
 {
     public class PiskelChunk
@@ -84,12 +92,19 @@ namespace Launchpad.NET.Effects
             {
                 Layers[index] = new PiskelLayer(layersData[index].ToString());
             }
+
+            Debug.WriteLine($"Successfully loaded piskel file for {Name}");
         }
     }
 
     public class PiskelEffect : ILaunchpadEffect
     {
-        List<JObject> layers;
+        int currentFrameIndex;
+        SKBitmap bitmap;
+        bool isInitiated;
+        LaunchpadMk2 launchpad;
+        List<Color[,]> frames;
+        PiskelFile piskelFile;
         public string Name => "Piskel";
 
         public IObservable<int> WhenChangeUpdateFrequency => null;
@@ -98,35 +113,83 @@ namespace Launchpad.NET.Effects
 
         public PiskelEffect(string filePath)
         {
-            layers = new List<JObject>();
+            frames = new List<Color[,]>();
             try
             {
-                var img = new BitmapImage();
-                Task.Run(async () =>
+                CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
                 {
                     var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(filePath);
-
-                    //var stream = await file.OpenAsync(FileAccessMode.Read);
-
-                    //var a = new Image();
-                    //a.
-                    //img.SetSource(stream);
-
-
                     var fileStream = File.OpenText(filePath);
                     var contents = fileStream.ReadToEnd();
-                    var piskelFile = new PiskelFile(contents);
-                    
+                    piskelFile = new PiskelFile(contents);
+                    var pngData = piskelFile.Layers.First().Chunks.First().Data;
+                    bitmap = SKBitmap.Decode(pngData);
+
+                    var totalFrames = piskelFile.Layers.First().FrameCount;
+                    var frameWidth = bitmap.Width / totalFrames;
+
+                    for (var frameIndex = 0; frameIndex < totalFrames; frameIndex++)
+                    {
+                        var frame = new Color[frameWidth, bitmap.Height];
+                        var xOffset = frameIndex * frameWidth;
+                        for (var y = 0; y < bitmap.Height; y++)
+                        {
+                            for (var x = 0; x < frameWidth; x++)
+                            {
+                                var pixel = bitmap.Pixels[frameWidth * y + (x + xOffset)];
+                                frame[x, y] = Color.FromArgb(pixel.Alpha, pixel.Red, pixel.Green, pixel.Blue);
+                            }
+                        }
+                        frames.Add(frame);
+                    }
 
 
+                    isInitiated = true;
                 });
+
             }
-            catch { }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        void DrawFrame(Color[,] frame)
+        {
+            var totalFrames = piskelFile.Layers.First().FrameCount;
+            var frameWidth = bitmap.Width / totalFrames;
+
+            for (var y = 0; y < frameWidth; y++)
+                for(var x=0; x< frameWidth; x++)
+                {
+                    launchpad.GridBuffer[x, y] = frame[x,y];
+                }
+            launchpad.FlushGridBuffer();
+        }
+
+        public async static Task<BitmapImage> ImageFromBytes(Byte[] bytes)
+        {
+            try
+            {
+                var image = new BitmapImage();
+                using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+                {
+                    await stream.WriteAsync(bytes.AsBuffer());
+                    stream.Seek(0);
+                    await image.SetSourceAsync(stream);
+                }
+                return image;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
         }
 
         public void Initiate(Launchpad launchpad)
         {
-            
+            this.launchpad = launchpad as LaunchpadMk2;
         }
 
         public void Terminate()
@@ -136,7 +199,12 @@ namespace Launchpad.NET.Effects
 
         public void Update()
         {
-            
+            if (!isInitiated) return;
+            currentFrameIndex++;
+            if (currentFrameIndex >= piskelFile.Layers.First().FrameCount) currentFrameIndex = 0;
+
+            DrawFrame(frames[currentFrameIndex]);
         }
     }
 }
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
